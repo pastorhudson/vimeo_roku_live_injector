237421af7b590c4dd4d336cfdb86afb57796ddee
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-11-10 22:07:23 -0500

    Add gunicorn to requirements.txt

---
+++ get_url.py
@@ -0,0 +1,60 @@
+import youtube_dl
+import os
+import requests
+
+
+def get_live_url(event_id):
+    url = f"https://vimeo.com/{event_id}"
+    ydl_opts = {}
+    ydl_opts = {
+        # 'outtmpl': '%(title)s.%(ext)s',
+        'format': ' bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4',
+
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        meta = ydl.extract_info(
+            url, download=False)
+        formats = meta.get('formats', [meta])
+    for f in formats:
+        # print(f)
+        try:
+            # if 'hls' in f['format'] and f['ext'] == 'mp4' and f['vcodec'] != 'none' and f['acodec'] != 'none' and f['fps'] == 30 and f['width'] == 1920:
+            if f['format_id'] == 'hls-fastly_live-2959':
+                # print(f'{f}\n\n')
+                return f['manifest_url']
+        except KeyError:
+            pass
+
+
+def get_vimeo_roku_feed():
+    url = os.environ.get('ROKU_FEED')
+    response = requests.get(url)
+    return response.json()
+
+
+def get_injected_roku_feed(event_id):
+    live_url = get_live_url(event_id)
+    live_object = {"id": "20201108",
+                   "title": "Live Stream",
+                   "shortDescription": "Worship with us each Sunday at 10:00 AM EST.",
+                   "thumbnail": "https://s3.amazonaws.com/hproku/images/live-stream-1280.png",
+                   "genres": ["faith"], "tags": ["live"], "releaseDate": "2016-06-01",
+                   "content": {"dateAdded": "2020-02-07T14:14:54.431Z",
+                               "captions": [],
+                               "duration": 230,
+                               "videos": [{"url": live_url,
+                                           "quality": "HD",
+                                           "videoType": "HLS"}]}}
+
+    cur_roku_feed = get_vimeo_roku_feed()
+    cur_roku_feed['movies'].append(live_object)
+
+    return cur_roku_feed
+
+
+if __name__ == "__main__":
+    print(get_live_url(477782549))
+    # print(get_injected_roku_feed(477782549))
+    # https: // vimeo.com / 477782549
+    pass
+
---
+++ roku_feed.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from get_url import get_injected_roku_feed
+import dotenv
+dotenv.load_dotenv(dotenv_path='secrets.env')
+app = Flask(__name__)
+
+
+@app.route('/')
+def channel_feed():
+    return get_injected_roku_feed(477782549)
+
+
+if __name__ == '__main__':
+    app.run()
