46013c50801f3285836fe29ae2d910f7679a4ead
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-11-11 15:26:28 -0500

    Fixed Live manifest injector

--- get_url.py
+++ get_url.py
@@ -12,10 +12,10 @@ def get_live_url(url=None):
     else:
         url = url
         print(url)
-    ydl_opts = {}
+    # ydl_opts = {}
     ydl_opts = {
         # 'outtmpl': '%(title)s.%(ext)s',
-        'format': ' bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4',
+        # 'format': ' bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4',
 
     }
     try:
@@ -23,17 +23,15 @@ def get_live_url(url=None):
             meta = ydl.extract_info(
                 url, download=False)
             formats = meta.get('formats', [meta])
-
+            tbr = 1
+            best = None
             for f in formats:
-                # print(f)
-                try:
-                    if f['format_id'] == 'hls-fastly_live-2959':
-                        # print(f'{f}\n\n')
-                        return f
-                    if 'hls' in f['format'] and f['ext'] == 'mp4' and f['vcodec'] != 'none' and f['acodec'] != 'none' and f['fps'] == 30 and f['width'] == 1920:
-                        return f
-                except KeyError:
-                    pass
+                if 'audio' not in f['format_id'] and f['tbr']:
+                    if int(f['tbr']) > tbr:
+                        best = f
+                        tbr = int(f['tbr'])
+            return best
+
     except youtube_dl.utils.DownloadError:
         return None
 
@@ -51,7 +49,7 @@ def get_injected_roku_feed():
     # if live_blob:
     est = timezone('US/Eastern')
     now = datetime.now(tz=Eastern)
-    end_time = datetime.now(tz=Eastern) + timedelta(hours=2)
+    end_time = datetime.now(tz=Eastern) + timedelta(hours=24)
     # 2020-11-08T19:04:55-05:00 <- Expected Time format for Roku Direct Publisher
     now = datetime.strftime(now, '%Y-%m-%dT%H:%M:%S%z')
     end_time = datetime.strftime(end_time, '%Y-%m-%dT%H:%M:%S%z')
@@ -69,15 +67,15 @@ def get_injected_roku_feed():
     #                "validityPeriodStart": now,
     #                "validityPeriodEnd": end_time}
 
-    live_object = {"id": "cbctest2",
+    live_object = {"id": "cbctest3",
                    "title": "Live Stream",
                    "shortDescription": "Worship with us each Sunday at 10:00 AM EST.",
                    "thumbnail": "https://images.squarespace-cdn.com/content/v1/5000d51de4b0392912a47ef2/1605108881444-GSZYHESIKEAT6TLX8TS2/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UbeDbaZv1s3QfpIA4TYnL5Qao8BosUKjCVjCf8TKewJIH3bqxw7fF48mhrq5Ulr0Hg/cbclive.png",
                    "genres": ["faith"], "tags": ["live"], "releaseDate": "2020-11-10",
-                   "content": {"dateAdded": "2020-11-10T14:14:54.431Z",
+                   "content": {"dateAdded": "2020-11-11T14:14:54.431Z",
                                "captions": [],
                                "duration": 5000,
-                               "videos": [{"url": "https://live.vimeocdn.com/1605114253-0x361467dc53008ed25973f55becd32ba9a6df8375/cbe7ada7-4f26-43b7-9362-eebf7c2435bd/hls.m3u8",
+                               "videos": [{"url": "https://25b9273ec647.ngrok.io/live.m3u8",
                                            "quality": "HD",
                                            "videoType": "HLS"}]},
                    "validityPeriodStart": now,
@@ -88,9 +86,38 @@ def get_injected_roku_feed():
     return cur_roku_feed
 
 
+def get_offline_content():
+    offline_content = {
+        "id": "478125860",
+        "title": "Live Stream Offline",
+        "shortDescription": "Find hope every Sunday at 10am with Calvary Baptist Church.",
+        "thumbnail": "https://i.vimeocdn.com/video/992886664_800x450.png",
+        "releaseDate": "2020-11-11T12:35:03-05:00",
+        "genres": [
+            "faith"
+        ],
+        "tags": [
+            "faith"
+        ],
+        "content": {
+            "dateAdded": "2020-11-11T13:22:51-05:00",
+            "duration": 24,
+            "videos": [
+                {
+                    "url": "https://player.vimeo.com/external/478125860.hd.mp4?s=017c1c0870b85022036b83fdadaa7a2e36a6875c&profile_id=175",
+                    "quality": "FHD",
+                    "videoType": "MP4",
+                    "bitrate": 4860265
+                }
+            ]
+        }
+    }
+    return "https://143vod-adaptive.akamaized.net/exp=1605126373~acl=%2Fea26be3b-4680-4ce4-b24b-d5df9cb78c7e%2F%2A~hmac=a1f84823cadb5590d868cf88ec4250e2bb8b88a841b2dff47c8a606c187e569d/ea26be3b-4680-4ce4-b24b-d5df9cb78c7e/video/f2940ff3/playlist.m3u8"
+
+
 if __name__ == "__main__":
-    # print(get_live_url('https://vimeo.com/476960836'))
-    print(get_live_url())
+    # print(get_live_url('https://vimeo.com/478125860'))
+    print(get_live_url('https://vimeo.com/478184394'))
 
     # print(get_injected_roku_feed())
     # https: // vimeo.com / 477782549
--- roku_feed.py
+++ roku_feed.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect
-from get_url import get_injected_roku_feed, get_live_url
+from get_url import get_injected_roku_feed, get_live_url, get_offline_content
 import dotenv
 import os
 dotenv.load_dotenv(dotenv_path='secrets.env')
@@ -13,12 +13,13 @@ def channel_feed():
 
 @app.route('/live.m3u8')
 def live():
+    # return redirect(get_offline_content(), 302)
     live_feed_url = get_live_url()
     if live_feed_url:
-        return redirect(live_feed_url['url'], code=302)
+        return redirect(live_feed_url['manifest_url'], code=302)
     else:
         offline_url = get_live_url(os.environ.get('OFFLINE_URL'))
-        return redirect(offline_url['url'], code=302)
+        return redirect(offline_url['url'], code=301)
 
 
 if __name__ == '__main__':
